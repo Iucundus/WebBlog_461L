e7ff7114ca158749a86a3bb0b8ca18a29dc75d3e
John Koelling <john.koelling@utexas.edu>
2019-03-04 13:58:15 -0600

    Initial commit
    Copy of Tutorial 6 code

---
+++ src/guestbook/OfySignGuestbookServlet.java
@@ -0,0 +1,38 @@
+// https://ee461lt6.appspot.com/ofyguestbook.jsp
+package guestbook;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.googlecode.objectify.ObjectifyService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
+public class OfySignGuestbookServlet extends HttpServlet {
+
+    static {
+        ObjectifyService.register(Greeting.class);
+    }
+
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+
+        String guestbookName = req.getParameter("guestbookName");
+        Key guestbookKey = KeyFactory.createKey("guestbook.Guestbook", guestbookName);
+        String content = req.getParameter("content");
+        Date date = new Date();
+
+        Greeting greeting = new Greeting(user, content, guestbookName);
+        ofy().save().entity(greeting).now();   // synchronous
+        resp.sendRedirect("/ofyguestbook.jsp?guestbookName=" + guestbookName);
+    }
+
+}
---
+++ src/guestbook/SignGuestbookServlet.java
@@ -0,0 +1,49 @@
+package guestbook;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+
+import java.io.IOException;
+import java.util.Date;
+
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class SignGuestbookServlet extends HttpServlet {
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        UserService userService = UserServiceFactory.getUserService();
+        User user = userService.getCurrentUser();
+
+
+        // We have one entity group per guestbook.Guestbook with all Greetings residing
+        // in the same entity group as the guestbook.Guestbook to which they belong.
+        // This lets us run a transactional ancestor query to retrieve all
+        // Greetings for a given guestbook.Guestbook.  However, the write rate to each
+        // guestbook.Guestbook should be limited to ~1/second.
+        String guestbookName = req.getParameter("guestbookName");
+        Key guestbookKey = KeyFactory.createKey("guestbook.Guestbook", guestbookName);
+        String content = req.getParameter("content");
+        Date date = new Date();
+        Entity greeting = new Entity("Greeting", guestbookKey);
+        greeting.setProperty("user", user);
+        greeting.setProperty("date", date);
+        greeting.setProperty("content", content);
+
+
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        datastore.put(greeting);
+
+
+        resp.sendRedirect("/guestbook.jsp?guestbookName=" + guestbookName);
+    }
+}
